1aa75abd250eea25f3856f9267767546dc1cde8b
Sudokamikaze <sudokamikaze@protonmail.com>
2018-06-15 18:07:36 +0300

    Move out of class ping() function and remove unneeded piece of code & also correct docker syntax usage
    Signed-off-by: Sudokamikaze <sudokamikaze@protonmail.com>

--- main.py
+++ main.py
@@ -13,18 +13,9 @@ class MAINC:
     __repo_url = "https://github.com/Sudokamikaze/Slave.git"
     __repo_dir = "/tmp/Slave"
     __config_file = "env.config"
-    __Jsecret = ""
-    __Jn_Name = ""
-    __Jm_IP = ""
-    __Jm_Port = ""
+    __container = client.containers.get('Jenkins_slave')
 
     def __init__(self):
-        try:
-            client.ping()
-        except docker.errors.APIError:
-            print('No connection to Docker Engine, exiting...')
-            os.sys.exit(1)
-
         self.__parser.read(self.__config_file)
         self.check_for_updates()
 
@@ -33,31 +24,20 @@ class MAINC:
             print('Clonning repo')
             git.Repo.clone_from("https://github.com/Sudokamikaze/Slave.git", self.__repo_dir)
             print('Starting container update procedure')
-            self.calling_docker()
-
         else:
-            if self.puller() == "Nothing to do":
+            if git.cmd.Git(self.__repo_dir).pull() == "Already up to date.":
                 print('There is nothing to do, exiting...')
                 os.sys.exit(0)
             else:
                 print('Starting container update procedure')
-                self.calling_docker()
+        self.calling_docker()
 
-    def puller(self):
-        if git.cmd.Git(self.__repo_dir).pull() == "Already up to date.":
-            valuetoreturn = "Nothing to do"
-        else:
-            valuetoreturn = "Repo is updated"
-        
-        return valuetoreturn
-         
     def calling_docker(self):
-        container = client.containers.get('Jenkins_slave')
-        container.stop()
-        container.remove()
-        client.images.remove('slave')
+        self.__container.stop()
+        self.__container.remove()
+        client.images.remove('slave:latest')
         try:
-            client.images.build(path='/tmp/Slave', nocache=True, tag='slave:latest',
+            client.images.build(path=self.__repo_dir, nocache=True, tag='slave:latest',
             buildargs={
             'Jenkins_Secret': self.__parser['DEFAULT']['Jenkins_Secret'],
             'Jenkins_Node_Name': self.__parser['DEFAULT']['Jenkins_Node_Name'],
@@ -66,17 +46,23 @@ class MAINC:
                       }
             ) 
         except docker.errors.BuildError:
-            print('There is something went wrong')
+            print('There is something went wrong.')
             os.sys.exit(1)
         
-        client.containers.run(image='slave', detach=True, name='Jenkins_slave', network_mode='bridge', 
+        client.containers.run(image='slave:latest', detach=True, name='Jenkins_slave', network_mode='bridge', 
         volumes={
-        '/home/jenkins': {
-            'bind': 'slave_data', 
+        'slave_data': {
+            'bind': '/home/jenkins', 
             'mode': 'rw'
             }
         })
         
         print('Finished updaing Docker container! Git HEAD was {}'.format(git.Repo(self.__repo_dir).repo.head.commit))
 
-classcall = MAINC()
+try:
+    client.ping()
+except docker.errors.APIError:
+    print('No connection to Docker Engine, exiting...')
+    os.sys.exit(1)
+else:
+    classcall = MAINC()
